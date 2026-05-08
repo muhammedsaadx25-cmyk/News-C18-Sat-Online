bc12de0a17b5ed5922bc4eae6f8f67cb01891797
omer1998 <omerfaris11@gmail.com>
2026-05-09 00:46:35 +0300

    add onClick callback to article item make article item clickable using InkWell

--- lib/features/home/views/sources_view/article_item.dart
+++ lib/features/home/views/sources_view/article_item.dart
@@ -7,34 +7,38 @@ import 'package:news/core/resources/assets_manager.dart';
 import 'package:news/core/resources/colors_manager.dart';
 
 class ArticleItem extends StatelessWidget {
-   ArticleItem({super.key, required this.article});
+   ArticleItem({super.key, required this.article, required this.onClick});
 Article article;
+void Function(Article) onClick;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: REdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.r),
-        border: Border.all(color: ColorsManager.white, width: 1.w)
-      ),
-      child: Column(
-        children: [
-          CachedNetworkImage(
-            imageUrl: article.urlToImage ?? '',
-            placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-          ),
-          SizedBox(height: 10.h,),
-          Text(article.title ?? '', style: GoogleFonts.inter(fontWeight: FontWeight.bold, fontSize: 16.sp, color: ColorsManager.white),)
-          ,SizedBox(height: 10.h,),
-          Row(
-            children: [
-              Expanded(child: Text(article.author ?? '', style: GoogleFonts.inter(fontWeight: FontWeight.w500, fontSize: 12.sp, color: ColorsManager.grey),))
-              ,Text(article.publishedAt ?? '', style: GoogleFonts.inter(fontWeight: FontWeight.w500, fontSize: 12.sp, color: ColorsManager.grey),)
+    return InkWell(
+      onTap: () => onClick(article),
+      child: Container(
+        padding: REdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.r),
+          border: Border.all(color: ColorsManager.white, width: 1.w)
+        ),
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              imageUrl: article.urlToImage ?? '',
+              placeholder: (context, url) => Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+            SizedBox(height: 10.h,),
+            Text(article.title ?? '', style: GoogleFonts.inter(fontWeight: FontWeight.bold, fontSize: 16.sp, color: ColorsManager.white),)
+            ,SizedBox(height: 10.h,),
+            Row(
+              children: [
+                Expanded(child: Text(article.author ?? '', style: GoogleFonts.inter(fontWeight: FontWeight.w500, fontSize: 12.sp, color: ColorsManager.grey),))
+                ,Text(article.publishedAt ?? '', style: GoogleFonts.inter(fontWeight: FontWeight.w500, fontSize: 12.sp, color: ColorsManager.grey),)
 
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
